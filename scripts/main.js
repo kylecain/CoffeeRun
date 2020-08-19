f28174449069c7e5269affa2b587fa5b9b6ce702
Kyle Cain <kylecain0305@gmail.com>
2020-08-18 22:00:29 -0500

    added a little bit of validation to the form

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@
     var Truck = App.Truck;
     var DataStore = App.DataStore;
     var FormHandler = App.FormHandler;
+    var Validation = App.Validation;
     var CheckList = App.CheckList;
 
     var myTruck = new Truck('ncc-1701', new DataStore());
@@ -19,5 +20,6 @@
         checklist.addRow.call(checklist, data);
     });
 
+    formHandler.addInputHandler(Validation.isCompanyEmail);
     console.log(formHandler);
 })(window);
