86945225aa1be3e409f806079ff2db5a34ee1bc4
Kyle Cain <kylecain0305@gmail.com>
2020-08-18 20:34:04 -0500

    added pending orders and made listeners/handlers for when a user wants to delete an order

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,23 @@
 (function (window){
     'use strict';
-    var FORM_SELECTOR = '[data-coffee-order="form"]'
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]'
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
     var FormHandler = App.FormHandler;
+    var CheckList = App.CheckList;
 
     var myTruck = new Truck('ncc-1701', new DataStore());
+    var checklist = new CheckList(CHECKLIST_SELECTOR)
+    checklist.addClickHandler(myTruck.deliverOrder.bind(myTruck))
     window.myTruck = myTruck;
 
     var formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    formHandler.addSubmitHandler(function(data) {
+        myTruck.createOrder.call(myTruck, data);
+        checklist.addRow.call(checklist, data);
+    });
+
     console.log(formHandler);
 })(window);
